d34e6d01fa05cc4690207a9a9edeafb5e30e4d27
mattqgoldberg <matt.q.goldberg@gmail.com>
2022-11-01 22:13:19 -0700

    added files

---
+++ zAppe/MainPage.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Plugin.BLE;
+using Plugin.BLE.Abstractions.Contracts;
+using Xamarin.Forms;
+
+namespace zAppe
+{
+    public partial class MainPage : ContentPage
+    {
+        IBluetoothLE ble;
+        IAdapter adapter;
+        public MainPage()
+        {
+            InitializeComponent();
+            ble = CrossBluetoothLE.Current;
+            BluetoothState State = ble.State;
+            state.Text = State.ToString();
+
+            ble.StateChanged += (s, e) =>
+            {
+                State = ble.State;
+                state.Text = State.ToString(); ;
+            };
+        }
+
+
+    }
+}
